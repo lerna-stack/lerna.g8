51d2f2f36e0a68d937a5c0e451c546928a7284ed
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-06-23 10:26:14 +0900

    Update ScalaTest to 3.1.4

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akka                     = "2.6.8"
     val akkaHttp                 = "10.1.12"
     val akkaPersistenceCassandra = "1.0.1"
-    val scalaTest                = "3.0.9"
+    val scalaTest                = "3.1.4"
     val airframe                 = "20.9.0"
     val logback                  = "1.2.3"
     val slick                    = "3.3.2"
