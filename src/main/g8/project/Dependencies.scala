b2cc17f732d21ebd7fc728bb18e8bb8ad053329d
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-06-24 14:30:25 +0900

    Use akka-actor-typed instead
    It is great to use Akka Typed for a new project.

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   object Akka {
     val slf4j            = "com.typesafe.akka" %% "akka-slf4j"              % Versions.akka
-    val actor            = "com.typesafe.akka" %% "akka-actor"              % Versions.akka
+    val actor            = "com.typesafe.akka" %% "akka-actor-typed"        % Versions.akka
     val stream           = "com.typesafe.akka" %% "akka-stream"             % Versions.akka
     val cluster          = "com.typesafe.akka" %% "akka-cluster-typed"      % Versions.akka
     val clusterTools     = "com.typesafe.akka" %% "akka-cluster-tools"      % Versions.akka
