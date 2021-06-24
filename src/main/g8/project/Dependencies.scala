21cf93d2eec1027d5f45a017f1beb43abbc9aebd
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-06-24 12:18:14 +0900

    Update Akka families to 2.6.12
    Generally, it is great to use the latest version.
    But we are great to pick 2.6.12 since akka-entity-replication recoomended us to use the same version that the library uses.
    We can check release notes the following links
    - https://akka.io/blog/news/2020/09/09/akka-2.6.9-released
    - https://akka.io/blog/news/2020/10/09/akka-2.6.10-released
    - https://akka.io/blog/news/2021/01/15/akka-2.6.11-released
    - https://akka.io/blog/news/2021/01/28/akka-2.6.12-released

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val lerna                    = "2.0.0-80f86b49-SNAPSHOT"
-    val akka                     = "2.6.8"
+    val akka                     = "2.6.12"
     val akkaHttp                 = "10.2.4"
     val akkaPersistenceCassandra = "1.0.1"
     val scalaTest                = "3.1.4"
